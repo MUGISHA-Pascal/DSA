20b7482bc84365c1ce1acd843629240a61330082
MUGISHA-Pascal <mugishapascal2008@gmail.com>
2024-12-06 11:47:11 +0200

    adding mian

---
+++ projects/employeeMIS/Main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include "Employee.hpp"
+#include "Department.hpp"
+#include "payroll.hpp"
+#include "Menu.hpp"
+#include "FileManager.hpp"
+
+using namespace std;
+int main(){
+    vector<Employee*> employees;
+    Department sales("Sales");
+    Payroll payroll;
+    FileManager::loadEmployeeData(employees);
+    while (true)
+    {
+        Menu::displayMenu();
+        Menu::handleInput(employees,sales,payroll);
+    }
+    return 0;
+}
