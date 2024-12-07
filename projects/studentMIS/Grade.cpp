100056065efb42b7be850b0ddae361a58b94288c
MUGISHA-Pascal <mugishapascal2008@gmail.com>
2024-12-07 20:54:02 +0200

    grade class

---
+++ projects/studentMIS/Grade.cpp
@@ -0,0 +1,27 @@
+#include "Grade.hpp"
+#include <iostream>
+using namespace std;
+
+void Grade::assignGrade(int studentID,int courseID,char grade){
+gradeBook[studentID][courseID]=grade;
+}
+char Grade::getGrade(int studentID,int courseID) const {
+    auto studentIt= gradeBook.find(studentID);
+    if(studentIt != gradeBook.end()){
+        auto courseIt = studentIt->second.find(courseID);
+     if(courseIt != studentIt -> second.end()){
+        return courseIt->second;
+     }
+    }
+    return "N";
+}
+void Grade::displayGrades(int studentID) const {
+    auto studentIt = gradeBook.find(studentID);
+    if(studentIt != gradeBook.end()){
+        for(const auto& [courseID,grade] : studentIt->second){
+            cout << "Course ID : "<< courseID << " Grade  : "<< grade << endl;
+        }
+    }else{
+        cout << "No grades found for student ID : " <<studentID <<endl;
+    }
+}
