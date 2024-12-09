65aa6f3b77447bb4bed259cf212b2ce51bee1d71
MUGISHA-Pascal <mugishapascal2008@gmail.com>
2024-12-09 22:07:26 +0200

    adding partition

--- sorting_algorithms/Combination1.cpp
+++ sorting_algorithms/Combination1.cpp
@@ -1,3 +1,6 @@
+#include <iostream>
+#include <vector>
+using namespace std;
 #int main(){
     while(true){
         int choice;
@@ -12,6 +15,18 @@
     switch (choice)
     {
         case 1:
+        int partition(vector<int>& arr , int low, int high){
+                int pivot = arr[high];
+                int i = low-1;
+                for(int j=low;j<high;j++){
+                    if(arr[j]<=pivot){
+                        i++;
+                        swap(arr[i],arr[j]);
+                    }
+                }
+                swap(arr[i+1],arr[high]);
+                return i+1;
+        }        
         break;
         case 2:
         break;
