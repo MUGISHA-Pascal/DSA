3f4514f7a44f498ce787e5a7da3e3a18cd0f0082
MUGISHA-Pascal <mugishapascal2008@gmail.com>
2024-12-09 17:53:51 +0200

    setting up

---
+++ sorting_algorithms/Combination1.cpp
@@ -0,0 +1,34 @@
+#int main(){
+    while(true){
+        int choice;
+    cout<<"Enter your choice : "<<endl;
+    cout<<"1. quick sort"<<endl;
+    cout<<"2. bubble sort"<<endl;
+    cout<<"3. merge sort"<<endl;
+    cout<<"4. selection sort"<<endl;
+    cout<<"5. insertion sort"<<endl;
+    cout<<"6. exit"<<endl;
+    cout<<"Enter the choice : "<<choice<<endl;
+    switch (choice)
+    {
+        case 1:
+        break;
+        case 2:
+        break;
+        case 3:
+        break;
+        case 4:
+        break;
+        case 5:
+        break;
+        case 6:
+        cout<<"Exiting....."<<endl;
+        exit(0);
+        break;
+    default:
+    cout << "invalid input"<<endl;
+        break;
+    }
+    }
+    return 0;
+}
