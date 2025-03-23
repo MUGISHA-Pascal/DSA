f3d5e0a13d492c048140b3ca7f67c38494ba905a
MUGISHA-Pascal <mugishapascal2008@gmail.com>
2025-03-23 20:28:58 +0200

    score sort

--- projects/score_sorting/score_sorting.cpp
+++ projects/score_sorting/score_sorting.cpp
@@ -14,7 +14,7 @@ void displayStudents(const vector<Student>& students){
         cout << "Name: "<< student.name<<" , Score: "<<student.score<<endl; 
     }
 }
-void bubblesort(vector<Student>& students){
+void bubbleSort(vector<Student>& students){
     int n = students.size();
     for(int i=0;i<n-1;++i){
         for(int j=0;j<n-i-1;++j){
@@ -24,7 +24,7 @@ void bubblesort(vector<Student>& students){
         }
     }
 }
-void selectionSort(vector<Students>& students){
+void selectionSort(vector<Student>& students){
     int n = students.size();
     for(int i=0;i<n-1;++i){
         int minIndex=i;
@@ -35,4 +35,58 @@ void selectionSort(vector<Students>& students){
         }
         swap(students[i],students[minIndex]);
     }
+}
+void insertionSort(vector<Student>& students){
+    int n=students.size();
+    for(int i=1;i<n;++i){
+        Student key=students[i];
+        int j= i-1;
+        while(j>=0 && students[j].score > key.score){
+            students[j+1]=students[j];
+            --j;
+        }
+        students[j+1]=key;
+    }
+}
+int main(){
+    vector<Student> students = {
+        {"Alice", 85},
+        {"Bob", 92},
+        {"Charlie", 78},
+        {"David", 90},
+        {"Eve", 88}
+    };
+
+    int choice;
+    cout << "Choose a sorting algorithm:\n";
+    cout << "1. Bubble Sort\n";
+    cout << "2. Selection Sort\n";
+    cout << "3. Insertion Sort\n";
+    cout << "Enter your choice: ";
+    cin >> choice;
+
+    cout << "\nBefore Sorting:\n";
+    displayStudents(students);
+
+    // Sort based on user choice
+    switch (choice) {
+        case 1:
+            bubbleSort(students);
+            cout << "\nAfter Sorting (Bubble Sort):\n";
+            break;
+        case 2:
+            selectionSort(students);
+            cout << "\nAfter Sorting (Selection Sort):\n";
+            break;
+        case 3:
+            insertionSort(students);
+            cout << "\nAfter Sorting (Insertion Sort):\n";
+            break;
+        default:
+            cout << "Invalid choice. Exiting...\n";
+            return 0;
+    }
+
+    displayStudents(students);
+    return 0;
 }
